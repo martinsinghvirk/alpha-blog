27c1d4593ac6b0b753ede1b9424d00285e00e3ea
Martin Singh <h15mavir@du.se>
2018-02-02 23:12:56 +0100

    Create articles table, model and add validations

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ActiveRecord::Base
+
+    # Validerar att en titel finns, innan en artikel lagras i db.
+    validates :title, presence :true, length: { minimum: 3, maximum: 50 }
+    validates :description, presence :true, length: { minimum: 10, maximum: 200 }
+    
+end
