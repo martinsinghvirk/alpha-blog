ed6f9e048547f924a8510077951928be02c70d78
Martin Singh <h15mavir@du.se>
2018-02-09 17:57:41 +0100

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,18 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
-        redirect_to articles_show(@article)
+        if @article.save
+            # Skriver ut ett status meddelande.
+            flash[:notice] = "Article was successfully created"
+            redirect_to article_path(@article)
+        else
+            # Kör metoden new.
+            render 'new'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ActiveRecord::Base
 
     # Validerar att en titel/beskrivning finns samt innehållslängd, innan en artikel lagras i db.
-    # validates :title, presence :true, length: { minimum: 3, maximum: 50 }
-    # validates :description, presence :true, length: { minimum: 10, maximum: 200 }
-    
+    validates_presence_of :title
+    validates_length_of :title, minimum: 3, maximum: 50
+
+    validates_presence_of :description
+    validates_length_of :description, minimum: 10, maximum: 200
 end
