e49a3f412d2f77b24f301fa345777a3dfb076ec4
Martin Singh <h15mavir@du.se>
2018-02-09 15:41:23 +0100

    Create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
 
-    # Validerar att en titel finns, innan en artikel lagras i db.
-    validates :title, presence :true, length: { minimum: 3, maximum: 50 }
-    validates :description, presence :true, length: { minimum: 10, maximum: 200 }
+    # Validerar att en titel/beskrivning finns samt innehållslängd, innan en artikel lagras i db.
+    # validates :title, presence :true, length: { minimum: 3, maximum: 50 }
+    # validates :description, presence :true, length: { minimum: 10, maximum: 200 }
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  # Ny väg för att utföra crud på artiklar.
+  resources :articles
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
