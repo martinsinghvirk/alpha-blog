ac35528d69d04503181a192526e5a2a0bcf8bdc9
Martin Singh <h15mavir@du.se>
2018-02-17 20:04:09 +0100

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
-class User < ApplicationRecord
-
+class User < ActiveRecord::Base
+    # Validering som kollar att unika värden förekommer samt längd för username och e-mail. För att kolla felkod ange t.ex. (user.errors.full_messages).
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 30 }
+    # Kollar formatet på angiven e-mail.
+    VALID_EMAIL_REGEX = /\A[\w+\-,]+@[a-z\d\-,][a-z]+\z/i
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 100 },
+    format: { with: VALID_EMAIL_REGEX }
 end
