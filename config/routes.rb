2aae1d59c4e005fd82b4e9fe7d656051faae2815
Martin Singh <h15mavir@du.se>
2018-03-17 11:26:50 +0100

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   # Ny väg för att utföra crud på artiklar.
   resources :articles
 
+  get 'signup', to: 'users#new'
+  resources :users, expect: [:new]  
+  #post 'users', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
