58a20a0d1827e74d54539af870cdd2266e9b4549
Martin Singh <h15mavir@du.se>
2018-03-17 10:58:07 +0100

    Add secure password and attribute to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ActiveRecord::Base
     # Gör den angiva e-mailen till små bokstäver innan den lagras i db. 
     before_save { self.email = email.downcase }
     # Validering som kollar att unika värden förekommer samt längd för username och e-mail. För att kolla felkod ange t.ex. (user.errors.full_messages).
-    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 30 }
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+        length: { minimum: 3, maximum: 30 }
     # Kollar formatet på angiven e-mail.
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 100 },
-    format: { with: VALID_EMAIL_REGEX }
-    validates :user_id, presence: true
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+        length: {maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
+    has_secure_password
 end
