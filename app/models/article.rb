590e8059de41bd83c45a0833ed6664ddbf14a888
Martin Singh <h15mavir@du.se>
2018-03-24 16:07:35 +0100

    Add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
     belongs_to :user
-    
+    has_many :article_categories #En artikel har många artikelkategorier.
+    has_many :categories, through: :article_categories #En artikel har många kategorier genom artikelkategorier.
+
     # Validerar att en titel/beskrivning finns samt innehållslängd, innan en artikel lagras i db.
     validates_presence_of :title
     validates_length_of :title, minimum: 3, maximum: 50
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ApplicationRecord
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+
+    # Validering.
     validates :name, presence: true, length: { minimum: 3, maximum: 25}
     validates_uniqueness_of :name
-
-
 
 end
